550c45ffc7a35e1ce7969d3e2907b60e21a76e69
Rawrskyes <rawrskyes@gmail.com>
2015-09-24 20:36:22 +1000

    update

--- Ability/Synth.cs
+++ Ability/Synth.cs
@@ -1,4 +1,5 @@
-﻿using ff14bot.Objects;
+﻿using ff14bot.Enums;
+using ff14bot.Objects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,38 @@ namespace crafty.Ability
         private const uint standard_synth = 100037;
         private const uint basic_synth = 100030;
         static uint[] ids = { 100037, 100030 };
+        static Class_Synths[] jobs = {
+            new Class_Synths(ClassJobType.Armorer, 100030, 100037),
+            new Class_Synths(ClassJobType.Alchemist, 100090, 100096),
+            new Class_Synths(ClassJobType.Blacksmith, 100015, 100021),
+            new Class_Synths(ClassJobType.Carpenter, 100001, 100007),
+            new Class_Synths(ClassJobType.Culinarian, 100105, 100111),
+            new Class_Synths(ClassJobType.Goldsmith, 100075, 100080),
+            new Class_Synths(ClassJobType.Leatherworker, 100045, 100051),
+            new Class_Synths(ClassJobType.Weaver, 100060, 100067)
+            };
+
+        public struct Class_Synths
+        {
+            public ClassJobType job;
+            public uint synth1;
+            public uint synth2;
+
+        public Class_Synths(ClassJobType job, uint synth1, uint synth2)
+            {
+                this.job = job;
+                this.synth1 = synth1;
+                this.synth2 = synth2;
+            }
+        }
+
+        public SpellData getBestBasic()
+        {
+            foreach(Class_Synths c in jobs)
+            {
+                return c;
+            }
+        }
 
 
         public static Composite UseSynth()
